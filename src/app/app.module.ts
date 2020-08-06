e03c03e3952deffd8c4fc6e65ecc348b72437dd9
RicardoPary <rikiricardo16@hotmail.com>
2020-08-05 23:42:05 -0400

    my-first-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { Test2Component } from './test2/test2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    Test2Component
   ],
   imports: [
     BrowserModule
