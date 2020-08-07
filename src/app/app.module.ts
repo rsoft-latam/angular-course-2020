c2f80f5bda2d6773b27ac12df2a9bea7205138be
RicardoPary <rikiricardo16@hotmail.com>
2020-08-07 14:47:21 -0400

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { Test2Component } from './test2/test2.component';
 import { Test3Component } from './test3/test3.component';
+import { Test10Component } from './test10/test10.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
     Test2Component,
-    Test3Component
+    Test3Component,
+    Test10Component
   ],
   imports: [
     BrowserModule
---
+++ src/app/test10/test10.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test10',
+  templateUrl: './test10.component.html',
+  styleUrls: ['./test10.component.css']
+})
+export class Test10Component implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
