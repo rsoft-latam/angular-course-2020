4a5c7bf11ac138df07192c95fd7638794e35e2d4
RicardoPary <rikiricardo16@hotmail.com>
2020-08-10 00:04:47 -0400

    google map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { TestComponent } from './test/test.component';
     TestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  ViewChild,
+  ElementRef
+} from "@angular/core";
+
+import { } from '@types/googlemaps';
 
 @Component({
-  selector: 'person',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  selector: "person",
+  templateUrl: "./test.component.html",
+  styleUrls: ["./test.component.css"]
 })
-export class TestComponent implements OnInit {
+export class TestComponent
+  implements
+    OnInit,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked {
+  @Input() name: string = "Ricardo";
 
-  constructor() { }
+  @ViewChild("gmap", { static: false }) gmap: ElementRef;
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  ngDoCheck() {
+    console.log("DO CHECK");
   }
 
+  ngAfterContentInit() {
+    console.log("AfterContentInit");
+  }
+
+  ngAfterContentChecked() {
+    console.log("AfterContentChecked");
+  }
+
+  ngAfterViewInit() {
+    console.log("AfterViewInit");
+
+    let map: google.maps.Map;
+    map = new google.maps.Map(this.gmap.nativeElement, {
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 8
+    });
+  }
+
+  ngAfterViewChecked() {
+    console.log("AfterViewChecked");
+  }
 }
