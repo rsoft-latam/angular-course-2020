f80f04190562d459b2cf6cbba5199edde11e295a
ricardo pari <rpari@kreadyf.com>
2020-08-20 15:37:58 -0400

    clean

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,96 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
-name = 'ricardo';
-
-aaaa(event){
-console.log('EVENTO DIERCGIVE:', event)
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  lastName = 'pari';
-
-
-
-
-
-
-
-
-
-  title = 'curso-angular';
-
-  numbers = [1,2,3,4,5,6,7,8,9,10];
-
-  name = 'ricardo';
-  lastName = 'pari';
-
-  persons = [
-    {
-      name: 'ricardo',
-      lastName: 'pari',
-      age: 22,
-      enable: true
-    },
-    {
-      name: 'marco',
-      lastName: 'marco',
-      age: 22,
-      enable: true
-    },
-    {
-      name: 'maria',
-      lastName: 'maria',
-      age: 22,
-      enable: false
-    },
-    {
-      name: 'juan',
-      lastName: 'juan',
-      age: 22,
-      enable: false
-    }
-  ]
-
-  auxNumber: number = 3;
-  auxExponent: number = 2;
-
-  ngOnInit(){
-    console.log('PURA: ', this.pura(6, 2));
-    console.log('IMPURA: ', this.impura(6, 2));
-  }
-
-  pura(a:number, b:number){
-    return a + b;
-  }
-
-  impura(a:number, b:number){
-    return a + b + Math.random();
+  ngOnInit(): void {
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,38 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 
-import { PersonaModule } from './modules/persona/persona.module';
-import { ProductoModule } from './modules/producto/producto.module';
-import { AdminModule } from './modules/admin/admin.module';
-import { UserModule } from './modules/user/user.module';
-import { TestPipe } from './pipes/test.pipe';
-import { ExpoPipe } from './pipes/expo.pipe';
-import { PurePipe } from './pipes/pure.pipe';
-import { ImpurePipe } from './pipes/impure.pipe';
-import { NgColorDirective } from './directives/ng-color.directive';
-import { TestComponent } from './components/test.component';
-import { NgFocusDirective } from './directives/ng-focus.directive';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TestPipe,
-    ExpoPipe,
-    PurePipe,
-    ImpurePipe,
-    NgColorDirective,
-    TestComponent,
-    NgFocusDirective
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    PersonaModule,
-    ProductoModule,
-
-    AdminModule,
-    UserModule
+    FormsModule
   ],
   providers: [
   ],
