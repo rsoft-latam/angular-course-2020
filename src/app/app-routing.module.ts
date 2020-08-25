8154bebdc6cea326d6c3f73daeb9e2bf90a208c6
RicardoPary <rikiricardo16@hotmail.com>
2020-08-25 15:36:01 -0400

    header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from './components/home/home.component';
-import {AdminComponent} from './components/admin/admin.component';
-import {AboutComponent} from './components/about/about.component';
-import {Home1Component} from './components/home1/home1.component';
-import {Home2Component} from './components/home2/home2.component';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    component: HomeComponent,
-    children: [
-      {path: 'home1', component: Home1Component},
-      {path: 'home2', component: Home2Component}
-    ]
-  },
-  {path: 'admin', component: AdminComponent},
-  {path: 'about', component: AboutComponent},
-  {path: 'user', loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule)}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
-import { AdminComponent } from './components/admin/admin.component';
 import {AppRoutingModule} from './app-routing.module';
-import { Home1Component } from './components/home1/home1.component';
-import { Home2Component } from './components/home2/home2.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
@@ -17,19 +12,13 @@ import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent,
-    AdminComponent,
-    Home1Component,
-    Home2Component
+    AppComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
     AppRoutingModule,
-    NgbModule,
 
     MatSliderModule,
     MatIconModule
