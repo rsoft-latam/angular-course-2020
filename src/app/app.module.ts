475c9ccd0dcdfc73e8af16337e3ff98ff03cf61f
ricardo pari <rpari@kreadyf.com>
2020-08-25 15:12:11 -0400

    angular materia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,10 @@ import {AppRoutingModule} from './app-routing.module';
 import { Home1Component } from './components/home1/home1.component';
 import { Home2Component } from './components/home2/home2.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MatSliderModule} from '@angular/material/slider';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -22,9 +26,13 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+
+    MatSliderModule,
+    MatIconModule
   ],
   providers: [
   ],
