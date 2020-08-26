39d3f80255e1dd83ac0e8fae9042bf66809abada
ricardo pari <rpari@kreadyf.com>
2020-08-26 15:11:04 -0400

    card angular material in home module

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import {HomeComponent} from './home/home.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
@@ -11,7 +13,9 @@ import {HomeComponent} from './home/home.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatCardModule,
+    MatButtonModule
   ]
 })
 export class HomeModule { }
--- src/app/pages/home/home/home.component.ts
+++ src/app/pages/home/home/home.component.ts
@@ -7,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  products = [
+    {
+      description: 'aaaaa',
+      imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId: 1,
+      price: 12,
+      title: 'saltenhas'
+    },
+    {
+      description: 'aaaaa',
+      imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId: 1,
+      price: 12,
+      title: 'saltenhas'
+    },
+    {
+      description: 'aaaaa',
+      imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId: 1,
+      price: 12,
+      title: 'saltenhas'
+    },
+    {
+      description: 'aaaaa',
+      imageUrl: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId: 1,
+      price: 12,
+      title: 'saltenhas'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
