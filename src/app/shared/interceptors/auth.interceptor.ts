30427284778b33ccadadb770215ef84380abc042
ricardo pari <rpari@kreadyf.com>
2020-09-10 15:04:05 -0400

    put token on interceptor

--- src/app/shared/interceptors/auth.interceptor.ts
+++ src/app/shared/interceptors/auth.interceptor.ts
@@ -18,7 +18,7 @@ export class AuthInterceptor implements HttpInterceptor {
 
     if (token) {
       request = request.clone({
-        url: `${request.url}`
+        url: `${request.url}?auth=${token}`
       });
     }
 
